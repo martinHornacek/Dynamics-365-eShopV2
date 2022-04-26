a50b99c66588eae86cdefa86dbb6f3247d799043
Martin Hornacek <martin.hornacek@accenture.com>
2022-04-26 13:50:38 +0200

    commneted out the prep db step

--- src/Services/Basket/Basket.API/Startup.cs
+++ src/Services/Basket/Basket.API/Startup.cs
@@ -66,7 +66,7 @@ namespace Basket.API
                 endpoints.MapControllers();
             });
 
-            PrepareDb.Populate(app);
+            //PrepareDb.Populate(app);
         }
     }
 }
